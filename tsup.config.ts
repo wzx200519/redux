4c59d90490cee5efa505c94e439dd3ff88d0aeff
wang <1690703797@qq.com>
2026-06-05 02:20:29 +0000

    4.4.3

--- tsup.config.ts
+++ tsup.config.ts
@@ -47,7 +47,8 @@ const mangleErrorsTransform: Plugin = {
 export default defineConfig((options): Options[] => {
   const commonOptions: Options = {
     entry: {
-      redux: 'src/index.ts'
+      redux: 'src/index.ts',
+      immutable: 'src/immutable.ts'
     },
     esbuildPlugins: [mangleErrorsTransform],
     sourcemap: true,
